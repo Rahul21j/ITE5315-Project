a0286a136deb010167713049b042e1f9e95b713e
Rahul <rahul1353rkj@gmail.com>
2024-04-17 09:36:54 -0400

    Debug

--- app.js
+++ app.js
@@ -503,7 +503,7 @@ app.get("/api/Movies/:id", async (req, res) => {
     const response = await fetch(`${hostUrl}/graphql`, {
       method: 'POST',
       headers: {
-        // 'Content-Type': 'application/json',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ query }),
     });
